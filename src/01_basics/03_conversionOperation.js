afccefdd057609aab19f9e7e77f35a35e3a18ab8
Pranab Ghosh <pranabit6@gmail.com>
2024-03-12 00:37:34 +0530

    added operations between operators

--- src/01_basics/03_conversionOperation.js
+++ src/01_basics/03_conversionOperation.js
@@ -15,3 +15,36 @@ console.log(booleanIsLoggedIn) // true
 const someNumber = 33
 const stringNumber = String(someNumber)
 console.log(typeof stringNumber) // string
+
+// *************************** Operations ***************************
+
+const value = 3
+const negValue = -value
+console.log(negValue) // -3
+console.log(2 + 2) // 4
+console.log(2 - 3) // -1
+console.log(2 * 2) // 4
+console.log(2 / 2) // 1
+console.log(2 % 2) // 0
+console.log(2 ** 3) // 8
+
+const str1 = 'Hello'
+const str2 = ' John'
+const str3 = str1 + str2
+console.log(str3) // Hello John
+
+console.log('1' + 2) // 12
+console.log(1 + '2') // 12
+console.log('1' + 2 + 2) // 122
+console.log(1 + 2 + '2') // 32
+console.log(((3 + 4) * 5) % 3) // 2
+console.log(true) // true
+
+let num1, num2, num3
+// eslint-disable-next-line prefer-const
+num1 = num2 = num3 = 2 + 2
+console.log(num1, num2, num3) // 4 4 4
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter) // 101
