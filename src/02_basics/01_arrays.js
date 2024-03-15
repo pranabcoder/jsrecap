2771fb37210efe07070f81392e1d67f2799d905f
Pranab Ghosh <pranabit6@gmail.com>
2024-03-15 18:54:08 +0530

    added 01_arrays file

---
+++ src/02_basics/01_arrays.js
@@ -0,0 +1,9 @@
+// Example in arrays in js
+const myArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(myArr[0]) // 0
+const myHeros = ['Shaktiman', 'Nagraj']
+console.log(myHeros[1]) // Nagraj
+
+// Example - Another way to create array
+const myArr2 = new Array(10, 20.5, 'Hello', true)
+console.log(myArr2[2]) // Hello
